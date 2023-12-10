96866f853b892e71c912c13232440fbd1a927e13
mcdonaez <92563275+mcdonaez@users.noreply.github.com>
2023-12-10 15:14:24 -0800

    Added Perlin Noise and Height for Plane
    Fix file path on line 94 to get correct file location

--- src/plane/src/main.rs
+++ src/plane/src/main.rs
@@ -1,12 +1,40 @@
-
 use bevy::prelude::*;
 use bevy::render::mesh::Indices;
-//use bevy::render::mesh::VertexAttributeValues;
 use bevy::render::render_resource::PrimitiveTopology;
 
+//Ezra Code Start
+use noise::{Fbm, Perlin};
+use noise::utils::{NoiseMapBuilder, PlaneMapBuilder};
+use std::io::{stdout, Write};
+use image::{GenericImageView, DynamicImage, Luma};
+//Ezra Code End
 
+fn main() 
+{
+    //Ezra Code Start
+    // Print to terminal
+    let mut lock = stdout().lock();
+    writeln!(lock, "hello world").unwrap();
+    
+    // Generate height map
+    let fbm = Fbm::<Perlin>::new(0);
+    /*
+    PlaneMapBuilder::<_, 2>::new(&fbm)
+    .set_size(100, 100)
+    .set_x_bounds(-10000.0, 10000.0)
+    .set_y_bounds(-10000.0, 10000.0)
+    .build()
+    .write_to_file("fbm.png");
+    */
+    PlaneMapBuilder::<_, 2>::new(&fbm)
+            .set_size(5000, 5000)
+            .set_x_bounds(-1.0, 1.0)
+            .set_y_bounds(-1.0, 1.0)
+            .build()
+            .write_to_file("fbm.png");
 
-fn main() {
+    //Ezra Code End
+
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
@@ -14,12 +42,13 @@ fn main() {
 }
 
 
+
 fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut meshes: ResMut<Assets<Mesh>>,
-) {
-
+    mut meshes: ResMut<Assets<Mesh>>,) 
+    
+    {
     let red_material_handle = materials.add(StandardMaterial {
         base_color: Color::rgba(1.0, 0.0, 0.0, 0.5),
         alpha_mode: AlphaMode::Blend,
@@ -50,7 +79,6 @@ fn setup(
 
 fn create_plane_mesh(size: f32, subdivisions: i32) -> Mesh {
 
-
     let x_vertices = subdivisions + 2;
     let z_vertices = subdivisions + 2;
     let tot_vertices = (x_vertices * z_vertices) as usize;
@@ -62,14 +90,37 @@ fn create_plane_mesh(size: f32, subdivisions: i32) -> Mesh {
     let mut indices: Vec<u32> = Vec::with_capacity(tot_indices);
 
 
+    // Ezra Code Start
+    // Load the image of hightmap (FIX THIS!!!!!)
+    let img_path = r"src\example_images\fbm.png";
+    let img = image::open(img_path).expect("Failed to open image");
+        
+    // Extract pixel values as f32 numbers
+    let perlin_values: Vec<f32> = img
+        .pixels()
+        .map(|(_, _, pixel)| pixel[0] as f32 / 255.0)
+        .collect();
+
+    // Keep track of position in vector
+    let mut index = 0;
+    // Ezra Code End
+
     // if x_vertices = 2;  ----- do 0, 1
     for x in 0..x_vertices {
         for z in 0..z_vertices {
 
+            // Ezra Code Start
+            let current_value = perlin_values[index];
+            // Ezra Code End
+
             // xi, zi = [0, 1]
             let xi = x as f32 / (x_vertices - 1) as f32;
             let zi = z as f32 / (z_vertices - 1) as f32;
-            let yi = 0.0 as f32;
+            //let yi = 0.0 as f32;
+            // Ezra Code Start
+            let yi = current_value as f32;
+            // Ezra Code End
+
 
             // Edge on origin
             // new_x, new_z = [0, size]
@@ -80,21 +131,14 @@ fn create_plane_mesh(size: f32, subdivisions: i32) -> Mesh {
             // new_x, new_z = [- size/2, + size/2]
             let x_pos = (xi - 0.5) * size;
             let z_pos = (zi - 0.5) * size;
-            
-            // Setting y's
-            /* 
-                // Flat Plane
-                // let y_pos = 0.0;      
 
-                // x^2 curve
-                // let y_pos = x_pos * x_pos / 2.0;     
-            */
-
-            // PERLIN NOISEEEEE
-            let y_pos = perlin_noise_3d(x_pos, yi, z_pos);
+            // Ezra Code Start
+            //y_pos needs to be a pretty small number to not spike wild style
+            let y_pos = (yi - 0.5) / size;
+            //Ezra Code End
 
             // build vertices/positions via set of squares
-            //positions.push([xi * size, 0.0, zi * size]);
+            // positions.push([xi * size, 0.0, zi * size]);
             positions.push([x_pos, y_pos, z_pos]);
 
             // build normals
@@ -110,6 +154,11 @@ fn create_plane_mesh(size: f32, subdivisions: i32) -> Mesh {
 
             // build uvs
             uvs.push([xi, zi]);
+
+            // Ezra Code
+            // Increment position in the index
+            index = (index + 1) % perlin_values.len();
+            // Ezra Code
         }
     }
 
@@ -131,38 +180,8 @@ fn create_plane_mesh(size: f32, subdivisions: i32) -> Mesh {
             indices.push((bl) as u32);
             indices.push((tl) as u32);
             indices.push((tr) as u32);
-        /* 
-            // Triangle 1 xz 00-01-10
-            indices.push((x * z + z) as u32);
-            indices.push((x * z + (z + 1)) as u32);
-            indices.push(((x * z + 1) + z) as u32);
-
-            // Triangle 2 xz 10-01-11
-            indices.push(((x * z + 1) + z) as u32);
-            indices.push((x * z + (z + 1)) as u32);
-            indices.push(((x * z + 1) + (z + 1)) as u32);
-        */
         }
     }
-
-
-/* Bevy V 1.12.0
-    #[rustfmt::skip]
-    let plane_mesh = Mesh::new(PrimitiveTopology::TriangleList)
-        .with_inserted_attribute(
-            Mesh::ATTRIBUTE_POSITION,
-            positions,
-        )
-        .with_inserted_attribute(
-            Mesh::ATTRIBUTE_UV_0,
-            uvs,
-        )
-        .with_inserted_attribute(
-            Mesh::ATTRIBUTE_NORMAL,
-            normals,
-        )
-        .with_indices(Some(Indices::U32(indices)));
-*/
         
     let mut plane_mesh = Mesh::new(PrimitiveTopology::TriangleList);
     plane_mesh.set_indices(Some(Indices::U32(indices)));
@@ -179,52 +198,5 @@ fn create_plane_mesh(size: f32, subdivisions: i32) -> Mesh {
         Mesh::ATTRIBUTE_UV_0, 
         uvs);
 
-
-
     plane_mesh
-}
-
-
-// perlin noise heavily based on https://adrianb.io/2014/08/09/perlinnoise.html
-fn perlin_noise_3d (x: f32, y: f32, z: f32) -> f32 {
-
-    // 0-255 inclusive + unique + random
-    // Is this the seed???
-    // stolen from https://adrianb.io/2014/08/09/perlinnoise.html
-    let permutation = [ 151,160,137,91,90,15,
-        131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-        190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-        88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-        77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-        102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-        135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-        5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-        223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-        129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-        251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-        49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-        138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-    ];
-    
-    // array of 0's 
-    let mut p: [i32; 512] = [0; 512];
-
-    for x in 0..512-1 {
-        p[x] = permutation[x % 255];
-    }
-
-    // Which square (repeats after 255)
-    // get whole int [0, 255]
-    let xi = x as i32 & 255;
-    let yi = y as i32 & 255;
-    let zi = z as i32 & 255;
-    // Where in the square
-    // get decimals
-    let xf = (x - (xi ) as f32);
-    let yf = (y - (yi ) as f32);
-    let zf = (z - (zi ) as f32);
-
-    0.0
-    //(xi) as f32
-    //x * x
 }
